b4adc92b41c4cf22574b8cf6e2de4e47b67bf07c
landot <timowland@gmail.com>
2014-11-28 21:08:28 -0800

    finished assignment

--- js/app.js
+++ js/app.js
@@ -1,7 +1,20 @@
-/* 
-    app.js
-    Angular application for the address book challenge
 
-    Add code here to create a new Angular application and controller.
-    The array of employee objects is already in a global variable named 'pawneeEmployees'
-*/
+"use strict";
+
+angular.module('AddressBook', [])
+	.controller('AddressController', function($scope) {
+		$scope.employees = pawneeEmployees;
+		$scope.sortCol = 'lastName';	
+		$scope.sortBy = function(sortCol) {
+			if( $scope.sortCol == sortCol) {
+				$scope.sortReverse = !$scope.sortReverse;
+			}else {
+				$scope.sortCol = sortCol;
+				$scope.sortReverse = false;
+			}
+			$scope.sortCol = sortCol;
+		};
+		$scope.isSortedBy = function(colName) {
+			return colName ==$scope.sortCol;
+		};
+	});
